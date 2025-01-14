b24c657a69986c42cab958a8e1b5a71402919b61
JHOSEPH <jhoseph@localhost.localdomain>
2025-01-14 01:01:36 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SingleFile from './components/single-file.vue'
+</script>
+
+<template>
+  <SingleFile />
+</template>
+
+<style scoped></style>
---
+++ src/components/single-file.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+//logica app
+const saludo = ref('hola edteam')
+</script>
+
+<template>
+  <!-- html ==> la estructura del componente -->
+  <div>
+    {{ saludo }}
+  </div>
+</template>
+
+<style scoped>
+/* aqui estara el los estilos
+    y el scoped es para que los stilos que se pongo solo funcione en ese componente y si quitamos el scoped los estilos se pondran globalmente */
+</style>
