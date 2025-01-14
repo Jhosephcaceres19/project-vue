abf15f0d047428476f4740d10448d8f6c6bfe1f9
JHOSEPH <jhoseph@localhost.localdomain>
2025-01-14 01:10:59 -0400

    add: component reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import ReactivityFile from './components/reactivity-file.vue'
 import SingleFile from './components/single-file.vue'
 </script>
 
 <template>
   <SingleFile />
+  <ReactivityFile />
 </template>
 
 <style scoped></style>
---
+++ src/components/reactivity-file.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const message = 'hola jose estas en el curso de vue'
+</script>
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<style scoped></style>
